8d08a7136892e661dbc3e10337818a761619f030
telegrambotmake0 <yusufsamiinsta@gmail.com>
2026-05-05 17:34:48 +0300

    Use environment variable for web server port

--- bot.py
+++ bot.py
@@ -29,7 +29,8 @@ def home():
     return "Bot aktif!"
 
 def run_web():
-    app_web.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+app_web.run(host="0.0.0.0", port=port)
 
 def keep_alive():
     t = Thread(target=run_web)
